b55650b16df088397b5b6048d409ec5703d0c260
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-10-23 14:23:18 +0200

    form request

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -19,15 +20,15 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
         // Validators
-        $request->validate([
-            'title' => ['required', 'min:5', 'max:255'],
-            'slug' => 'required|unique:post',
-            'category' => 'required',
-            'content' => 'required',
-        ]);
+        // $request->validate([
+        //     'title' => ['required', 'min:5', 'max:255'],
+        //     'slug' => 'required|unique:post',
+        //     'category' => 'required',
+        //     'content' => 'required',
+        // ]);
 
         Post::create(
             // [
